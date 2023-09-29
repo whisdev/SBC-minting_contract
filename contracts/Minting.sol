727143064a6fb556dbd2aeaa76fd74241fced9e7
sakele1026 <sakelejames@gmail.com>
2023-09-30 03:14:10 +0900

    complete

--- contracts/Minting.sol
+++ contracts/Minting.sol
@@ -1,35 +1,32 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.9;
+pragma solidity ^0.8.14;
 
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "./Guardian/Erc721LockRegistry.sol";
-import "./interfaces/IERC721xHelper.sol";
 import "./OPR/upgradeable/DefaultOperatorFiltererUpgradeable.sol";
 
-contract SBCMinting is ERC721x, DefaultOperatorFiltererUpgradeable, IERC721xHelper, ERC721URIStorage, Ownable {
+contract SBCMinting is  DefaultOperatorFiltererUpgradeable, ERC721URIStorage, Ownable {
     
-    IERC721 SBCColletion;
+    // IERC721 SBCColletion;
 
     using Counters for Counters.Counter;
     using Counters for uint256;
-
+    
+    bool public lockingState = true;
     uint256 public MAX_SUPPLY;
     uint256 public totalMinted;
     address contractOwner;
 
-    mapping (uint256=>bool) sendNFTLocked;
-
     constructor(
         string memory _name,
-        string memory _symbol,
-        IERC721 _SBCCollection
+        string memory _symbol
+        // bool lockingState,
+        // IERC721 _SBCCollection
     ) ERC721(_name, _symbol) {
         contractOwner = msg.sender;
         MAX_SUPPLY = 1111;
         totalMinted = 0;
-        SBCColletion = _SBCCollection;
     }
 
     function minting(string memory _baseURI) external {
@@ -44,35 +41,31 @@ contract SBCMinting is ERC721x, DefaultOperatorFiltererUpgradeable, IERC721xHelp
     }
 
     function transferFrom(
+        address _from,
         address _to,
         uint256 _tokenId
-    ) public payable {
+    ) public override(IERC721, ERC721) {
         require(
-            sendNFTLocked[_tokenId] == false,
+            lockingState == false,
             "Cannot transfer - currently locked"
         );
-        super.transferFrom(contractOwner, _to, _tokenId);
+        super.transferFrom(_from, _to, _tokenId);
     }
 
     function safeTransferFrom(
+        address _from,
         address _to,
         uint256 _tokenId,
         bytes memory data
-    ) public payable {
+    ) public override(IERC721, ERC721) {
         require(
-            sendNFTLocked[_tokenId] == false,
+            lockingState == false,
             "Cannot transfer - currently locked"
         );
-        super.safeTransferFrom(contractOwner, _to, _tokenId, data);
+        super.safeTransferFrom(_from, _to, _tokenId, data);
     }
 
-    function setNFTLock (uint256 _tokenId) public onlyOwner{
-        require(SBCColletion.ownerOf(_tokenId) == msg.sender, "Not Owner");
-        sendNFTLocked[_tokenId] = true;
-    }
-
-    function setNFTUnLock (uint256 _tokenId) public onlyOwner {
-        require(SBCColletion.ownerOf(_tokenId) == msg.sender, "Not Owner");
-        sendNFTLocked[_tokenId] = false;
+    function modifyLockingState() external onlyOwner {
+        lockingState = true;
     }
 }
